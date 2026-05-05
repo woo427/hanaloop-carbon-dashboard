dab53f1f219e8b381fee2f8ce2d68e2969b1a709
김지우 <annejwkim00@naver.com>
2026-05-05 20:28:50 +0900

    style: 탄소배출량 및 배출계수 카드 UI 수정

--- components/dashboard/CurrentEmissionFactor.tsx
+++ components/dashboard/CurrentEmissionFactor.tsx
@@ -9,13 +9,21 @@ export default function CurrentEmissionFactor() {
   return (
     <Card className="w-full">
       <CardHeader>
-        <CardTitle>현재 배출계수</CardTitle>
+        <CardTitle className="font-bold">현재 배출계수</CardTitle>
+        <div className="text-xs text-gray-400">
+          PCF 계산에 적용 중인 최신 기준값입니다.
+        </div>
       </CardHeader>
       <CardContent>
         {currentEmissionFactor?.map((item) => (
-          <div key={item.id}>
-            <p>
-              {item.category} {item.coeff} {item.unit}
+          <div
+            key={item.id}
+            className="flex justify-between py-2 border-b last:border-b-0"
+          >
+            <p className="text-gray-700">{item.category}</p>
+            <p className="w-[150px]">
+              <span className="font-semibold text-lg pr-1">{item.coeff}</span>
+              <span className="text-xs text-gray-600"> {item.unit}</span>
             </p>
           </div>
         ))}
--- components/dashboard/PcfSummary.tsx
+++ components/dashboard/PcfSummary.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { useGetPcfSummary } from "@/hooks/dashboard/useGetPcfSummary";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 export default function PcfSummary() {
   const { data: pcfSummary } = useGetPcfSummary();
@@ -15,10 +9,18 @@ export default function PcfSummary() {
   return (
     <Card className="w-full">
       <CardHeader>
-        <CardTitle>{pcfSummary} kgCO₂e</CardTitle>
+        <CardTitle className="font-bold pb-5">탄소배출량 (PCF)</CardTitle>
       </CardHeader>
-      <CardContent />
-      <CardFooter>2025년 pcf 요약</CardFooter>
+      <CardContent>
+        <div className="flex justify-end items-center">
+          <p className="text-gray-500">총</p>
+          <p className="text-4xl font-bold pl-3">
+            {pcfSummary?.toLocaleString()}
+          </p>
+          <p className="text-gray-500 pl-1">tCO₂eq</p>
+        </div>
+      </CardContent>
+      <p className="border-b" />
     </Card>
   );
 }
